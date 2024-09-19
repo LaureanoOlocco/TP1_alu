7031689f46242dcb44b17838211d44b50d0e0239
LaureanoOlocco <laureanoolocco@gmail.com>
2024-09-19 13:12:31 -0300

    Se elimino el i_sw_op

--- rtl/top_alu.v
+++ rtl/top_alu.v
@@ -6,41 +6,38 @@ module top_alu
 (   
     input  wire                           clk      ,
     input  wire         [2           : 0] i_btn    ,
-    input  wire         [NB_OP   - 1 : 0] i_sw_op  ,
-    input  wire         [NB_DATA - 1 : 0] i_sw_data,
+    input  wire signed  [NB_DATA - 1 : 0] i_sw_data,
     output wire signed  [NB_DATA - 1 : 0] o_result      // Resultado
 );
 
-    wire [NB_DATA - 1 : 0] i_data_a; // preguntar esto al feli
-    wire [NB_DATA - 1 : 0] i_data_b;
-    wire [NB_OP   - 1 : 0] i_op    ;
+    reg [NB_DATA - 1 : 0] data_a; // preguntar esto al feli
+    reg [NB_DATA - 1 : 0] data_b;
+    reg [NB_OP   - 1 : 0] op    ;
 
-    assign i_data_a = i_sw_data;
-    assign i_data_b = i_sw_data;
-
+  
     // Instanciación de la ALU
     ALU #(
         .NB_DATA(NB_DATA),
         .NB_OP  (NB_OP  )
     ) alu_instance (
-        .i_data_a(i_data_a),
-        .i_data_b(i_data_b),
-        .i_op    (i_op    ),
+        .i_data_a(data_a  ),
+        .i_data_b(data_b  ),
+        .i_op    (op      ),
         .o_result(o_result)
     );
 
     always(posedge clk) begin
         
         if(i_btn[0])begin
-            i_data_a <= i_sw_data;
+            data_a <= i_sw_data;
         end
 
         else if (i_btn[1]) begin
-            i_data_b <= i_sw_data;
+            data_b <= i_sw_data;
         end
 
         else if(i_btn[2])begin
-            i_op <= i_sw_op;
+            op <= i_sw_data[NB_OP-1:0];          // Revisar para que en lugar de usar i sw op usar los de data pero 6 bits nomas, op queda igual porque en la alu es un input de 6 bits
         end
 
     end
