aeab605be8a2a9383f3b84a9a8b2c886c713ff87
Agus Palla <pallaagus001@gmail.com>
2024-09-30 01:33:36 -0300

    i_valid, i_rst y feedback funcionando

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,7 +1,7 @@
 // Implementar en FPGA una ALU:
 //  - La ALU debe ser parametrizable (bus de datos) para poder ser utilizada posteriormente
 //  - Validar el desarrollo con test bench
-//      - Debe incluir generacion de entradas aleatorias y codigo de chequeo
+//  - Debe incluir generacion de entradas aleatorias y codigo de chequeo
 //  - Simular el diseno usando vivado con analisis de tiempo
 
 //  ADD     100000
@@ -15,36 +15,40 @@
 
 module ALU 
 #(
-    parameter NB_DATA = 8,      // Cantidad de bits de data
+    parameter NB_DATA = 8,        // Cantidad de bits de data
     parameter NB_OP   = 6         // Cantidad de bits para la operación
 )
 (
+    input  wire                           i_valid,    // valid para cambiar la salida
     input  wire signed  [NB_DATA - 1 : 0] i_data_a,   // 8 bits para a
     input  wire signed  [NB_DATA - 1 : 0] i_data_b,   // 8 bits para b
     input  wire         [NB_OP   - 1 : 0] i_op    ,   // 8 bits para operador
     output wire  signed [NB_DATA - 1 : 0] o_result    // Salida de la alu
 );
  
-    reg signed [NB_DATA-1:0] result             ;
-
+    reg signed [NB_DATA-1:0] result                    ;
+    reg signed [NB_DATA-1:0] feedback = {NB_DATA{1'b0}};
 
 always @(*) 
 begin
-
+            
     case (i_op)
-        6'b100000: result = i_data_a +   i_data_b ; // Operación ADD
-        6'b100010: result = i_data_a -   i_data_b ; // Operación SUB
-        6'b100100: result = i_data_a &   i_data_b ; // Operación AND
-        6'b100101: result = i_data_a |   i_data_b ; // Operación OR
-        6'b100110: result = i_data_a ^   i_data_b ; // Operación XORi_op
-        6'b000011: result = i_data_a >>> i_data_b ; // Operación SRA
-        6'b000010: result = i_data_a >>  i_data_b ; // Operación SRL
-        6'b100111: result = ~(i_data_a | i_data_b); // Operación NOR
-        default:   result = {NB_DATA{1'b0}}       ; // Sino, todo 0
+        6'b100000: result <= i_data_a +   i_data_b ; // Operación ADD
+        6'b100010: result <= i_data_a -   i_data_b ; // Operación SUB
+        6'b100100: result <= i_data_a &   i_data_b ; // Operación AND
+        6'b100101: result <= i_data_a |   i_data_b ; // Operación OR
+        6'b100110: result <= i_data_a ^   i_data_b ; // Operación XORi_op
+        6'b000011: result <= i_data_a >>> i_data_b ; // Operación SRA
+        6'b000010: result <= i_data_a >>  i_data_b ; // Operación SRL
+        6'b100111: result <= ~(i_data_a | i_data_b); // Operación NOR
+        default:   result <= {NB_DATA{1'b0}}       ; // Sino, todo 0
+            
     endcase
-    
+
+    feedback <= i_valid ? result : feedback;
+   
 end
 
-    assign o_result = result;
+    assign o_result = i_valid ? result : feedback;
     
 endmodule
--- rtl/top_alu.v
+++ rtl/top_alu.v
@@ -4,31 +4,39 @@ module top_alu
     parameter NB_OP   = 6       // Cantidad de bits para la operación
 )
 (   
-    input  wire                           clk      ,
-    input  wire         [2           : 0] i_btn    ,
-    input  wire signed  [NB_DATA - 1 : 0] i_sw_data,
-    input  wire                           i_rst    ,
-    output wire signed  [NB_DATA - 1 : 0] o_led      // Resultado
+    input  wire                          clk      ,
+    input  wire                          i_valid  ,
+    input  wire        [2           : 0] i_btn    ,
+    input  wire signed [NB_DATA - 1 : 0] i_sw_data,
+    input  wire                          i_rst    ,
+    output wire signed [NB_DATA - 1 : 0] o_led      // Resultado
 );
 
-    reg [NB_DATA - 1 : 0] data_a; // preguntar esto al feli
-    reg [NB_DATA - 1 : 0] data_b;
-    reg [NB_OP   - 1 : 0] op    ;
+    reg signed [NB_DATA - 1 : 0] data_a; 
+    reg signed [NB_DATA - 1 : 0] data_b;
+    reg        [NB_OP   - 1 : 0] op    ;
 
-  
     // Instanciación de la ALU
     ALU #(
-        .NB_DATA(NB_DATA),
-        .NB_OP  (NB_OP  )
+        .NB_DATA(NB_DATA ),
+        .NB_OP  (NB_OP   )
     ) alu_instance (
-        .i_data_a(data_a),
-        .i_data_b(data_b),
-        .i_op    (op    ),
-        .o_result(o_led )
+        .i_data_a(data_a ),
+        .i_data_b(data_b ),
+        .i_op    (op     ),
+        .i_valid (i_valid),
+        .o_result(o_led  )
     );
 
-    always@(posedge clk) begin
+    always@(posedge clk or posedge i_rst) begin
 
+        if (i_rst) begin
+            data_a <= 0;
+            data_b <= 0;
+            op     <= 0;
+        end
+        
+        else begin
             if(i_btn[0])begin
                 data_a <= i_sw_data;
             end
@@ -38,12 +46,9 @@ module top_alu
             end
 
             else if(i_btn[2])begin
-                op <= i_sw_data[NB_OP-1:0];          // Revisar para que en lugar de usar i sw op usar los de data pero 6 bits nomas, op queda igual porque en la alu es un input de 6 bits
-                
+                op <= i_sw_data[NB_OP-1:0];
             end
-                
+        end    
     end    
 
-    
- 
 endmodule
